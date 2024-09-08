3ee15528a2a7dd2b932cb193c0dc1cf58999b05b
Viraj Tupe <virajtupe71@gmail.com>
2024-09-08 17:58:42 +0530

    Create server.js

---
+++ Task 1/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const ejs = require('ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/submit', (req, res) => {
+    const { name, email } = req.body;
+    res.send(`Received: ${name} with email ${email}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
